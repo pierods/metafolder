29ac879966cba8fae7d34a220b4a8a0cda6cd667
Piero de Salvia <8037686+pierods@users.noreply.github.com>
2024-03-07 15:33:46 +0100

    greatly improved cell display

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,14 @@ use std::collections::HashMap;
 use std::path::{Path};
 use std::fs;
 use gtk::prelude::*;
-use gtk::{glib, Application, ApplicationWindow, Grid, pango, Overlay};
+use gtk::{glib, Application, ApplicationWindow, Grid, pango, Align};
+use html_escape;
 
 const APP_ID: &str = "org.github.pierods.metafolder";
 
 fn main() -> glib::ExitCode {
-    // Create a new application
     let app = Application::builder().application_id(APP_ID).build();
-    // Connect to "activate" signal of `app`
     app.connect_activate(build_ui);
-    // Run the application
     app.run()
 }
 
@@ -54,8 +52,8 @@ fn draw_icons_as_grid(entries: HashMap<String, String>, grid: &Grid, width: i32)
     let mut r: i32 = 0;
     let mut c: i32 = 0;
 
-    for (k, v_) in entries {
-        let size = 100;
+    for (k, _v) in entries {
+        let size = 60;
         grid.attach(&make_cell(k, size), c, r, size, size);
         c += size;
         if c > width {
@@ -66,12 +64,12 @@ fn draw_icons_as_grid(entries: HashMap<String, String>, grid: &Grid, width: i32)
     }
 }
 
-fn make_cell(text: String, size : i32) -> Overlay {
-    let img = gtk::Image::from_file("/home/piero/temp/gtk4-rs/examples/clipboard/asset.png");
-    //let img = gtk::Image::from_icon_name("folder");
-    img.set_halign(gtk::Align::Center);
+fn make_cell(text: String, size : i32) -> gtk::Box {
+    //let img = gtk::Image::from_file("asset.png");
+    let img = gtk::Image::from_icon_name("folder");
     img.set_pixel_size(size);
 
+    let text = html_escape::encode_text(text.as_str()).to_string();
     let pango_string = String::from("<span font_size=\"small\">") + text.as_str() + "</span>";
     let txt = gtk::Label::new(Option::Some(pango_string.as_str()));
     txt.set_use_markup(true);
@@ -79,20 +77,20 @@ fn make_cell(text: String, size : i32) -> Overlay {
     txt.set_wrap(true);
     txt.set_wrap_mode(pango::WrapMode::WordChar);
     txt.set_lines(2);
-
     txt.set_justify(gtk::Justification::Center);
 
+    txt.set_halign(Align::Center);
+    // txt.set_valign(Align::End);
+    img.set_halign(Align::Center);
+    // img.set_valign(Align::Start);
 
-    txt.set_halign(gtk::Align::Center);
-    txt.set_valign(gtk::Align::End);
-
-
-    let desktop_icon = gtk::Overlay::new();
-    desktop_icon.set_child(Option::Some(&img));
-    desktop_icon.add_overlay(&txt);
+    let desktop_icon = gtk::Box::new(gtk::Orientation::Vertical, 10);
+    desktop_icon.set_homogeneous(false);
+    desktop_icon.set_spacing(3);
+    desktop_icon.append(&img);
+    desktop_icon.append(&txt);
 
     desktop_icon
-
 }
 
 fn try_desktop(home_path : &str) -> bool {
@@ -118,7 +116,7 @@ fn get_entries(p: String) -> HashMap<String, String>{
     let paths = fs::read_dir(p).expect("Impossible to get your home dir!");
 
     for path in paths {
-        let dir_entry = path.unwrap().path();
+        let dir_entry = path.expect("Impossible to get your home dir!").path();
         let file_name_opt = dir_entry.file_name();
         match file_name_opt {
             Some(f) => {
